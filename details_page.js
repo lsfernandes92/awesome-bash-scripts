0e3795b78f2ccb7524c1e2de581a2fc4c9f70776
lsfernandes92 <lsfernandes92@gmail.com>
2024-09-15 13:12:30 -0300

    Add details page

---
+++ details_page.js
@@ -0,0 +1,29 @@
+const params = new URLSearchParams(window.location.search);
+const q = params.get("q");
+const description = params.get("description");
+const category = params.get("category");
+
+const binNameElement = document.querySelector(".bin_name")
+const binDescriptionElement = document.querySelector(".bin_description")
+const binCategoryElement = document.querySelector(".bin_category")
+
+document.title += ` | ./${q}`;
+
+binNameElement.innerHTML = `./${q}`;
+binDescriptionElement.innerHTML = description
+binCategoryElement.innerHTML = category
+
+const showScriptContent = (binName) => {
+  const scriptContent = document.querySelector(".script_content")
+
+  fetch(`bins/${binName}`)
+    .then(response => response.text())
+    .then(data => {
+      scriptContent.innerHTML = `
+          <pre data-prefix="~"><code>${data}</code></pre>
+      `
+    })
+    .catch(error => console.error("Error fetching the file:", error))
+}
+
+showScriptContent(q)
--- main.js
+++ main.js
@@ -19,28 +19,25 @@ const binariesContainer = document.querySelector(".container__binaries_section-w
 const categoriesElements = document.querySelectorAll(".superior__item")
 const binariesElements = document.querySelectorAll(".binaries__item")
 
+const goToBinaryScriptPage = (binObject) => {
+  const bin = JSON.parse(decodeURIComponent(binObject));
+  
+  const params = new URLSearchParams({
+     q: bin.name,
+     description: bin.description,
+     category: bin.category
+  });
+  window.location.href = `details_page.html?${params.toString()}`;
+} 
+
 const listBinaries = () => {
-  const dialogHTML = `
-    <dialog id="my_modal_1" class="modal">
-      <div class="modal-box">
-        <div class="binary-card p-8 mockup-code">
-          <pre><code>Testing</code></pre>
-        </div>
-        <div class="modal-action">
-          <form method="dialog">
-            <!-- if there is a button in form, it will close the modal -->
-            <button class="btn">Close</button>
-          </form>
-        </div>
-      </div>
-    </dialog>
-  `
-  document.body.insertAdjacentHTML('beforeend', dialogHTML);
   binariesJson.forEach(obj => {
+    const serializedObj = encodeURIComponent(JSON.stringify(obj));
+
     binariesContainer.innerHTML += `
       <li class="binaries__item mockup-code flex flex-row">
         <div class="binary-card p-8">
-          <pre data-prefix="$"><code class="binary-name"><a href="#" name="${obj.name}" onclick="my_modal_1.showModal()">./${obj.name}</a></code></pre>
+          <pre data-prefix="$"><code class="binary-name"><a href="#" name="${obj.name}" onclick="goToBinaryScriptPage('${serializedObj}')">./${obj.name}</a></code></pre>
           <pre data-prefix=">" class="text-success"><code class="binary-description">${obj.description}</code></pre>
           <pre data-prefix=">" class="text-warning"><code class="binary-category">${obj.category}</code></pre>
         </div>
